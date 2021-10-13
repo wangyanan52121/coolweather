2dc3be3bdc4bb7d4ec72a995c11dc19b29a2ac5b
wangyanan <wang.yanan15@byd.com>
2021-10-13 13:44:20 +0800

    最后

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -167,6 +167,7 @@ public class WeatherActivity extends AppCompatActivity {
                 });
             }
         });
+        Log.d("WeatherActivity","执行loadBIngPic()");
         loadBingPic();
     }
 
